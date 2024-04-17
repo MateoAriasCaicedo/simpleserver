ee74f439512d00b3896ba41663e77985ea205452
Luis Mateo Arias Caicedo <luismateoariascaicedo@gmail.com>
2024-04-17 14:57:28 -0500

    now the simple web server is a file one

--- server.ts
+++ server.ts
@@ -1,5 +1,10 @@
-function handler(_req: Request) {
-    return new Response("Hello!");
+async function handler(_req: Request) {
+    const file = await Deno.open('./index.html');
+
+    const headers = new Headers();
+    headers.append('content-type', 'text/html');
+
+    return new Response(file.readable, {headers});
 }
 
 Deno.serve({port: 8000, hostname: "127.0.0.1"}, handler);
