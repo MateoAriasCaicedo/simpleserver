0430750125b19d200167402341d4e875df658cd5
Luis Mateo Arias Caicedo <luismateoariascaicedo@gmail.com>
2024-04-18 14:26:14 -0500

    solved exercises for the lesson

---
+++ excercises/exercise2.ts
@@ -0,0 +1,27 @@
+/*
+    1. Start a new Deno workspace and create a
+       server.ts file
+
+    2. Start a new Deno Server with Deno.serve
+       on port 9000
+
+    3. Respond to each request with JSON containing
+       the Method and URL:
+       Eg: { url: http://localhost:9000/hello,
+             method: "GET" }
+
+    4. Start your server using deno run and
+       Test this in both Insomnia and the Browser
+
+    *HINT Remember the Header type needed to do this
+          properly!
+*/
+
+function echoJsonHandler(request: Request) {
+  const body = { url: request.url, method: request.method };
+  return new Response(JSON.stringify(body), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
+Deno.serve({ port: 9000 }, echoJsonHandler);
